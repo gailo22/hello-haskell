007716125b63c1c9f23ed5dfd28a441ec759dfc5
gailo22 <gailo22@gmail.com>
2013-09-13 18:05:23 +0700

    updated

--- src/hello.hs
+++ src/hello.hs
@@ -15,7 +15,7 @@ average ns = sum ns `div` length ns
 
 
 sum' :: Num a => [a] -> a
-sum' [] = 0
+sum' []     = 0
 sum' (x:xs) = x + sum' xs
 
 
@@ -25,3 +25,21 @@ qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
                where
                  smaller = [a | a <- xs, a <= x]
                  larger  = [b | b <- xs, b > x]
+
+
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)  
+surface :: Shape -> Float  
+surface (Circle _ _ r) = pi * r ^ 2  
+surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1) 
+
+
+data Person = Person { firstName :: String  
+                     , lastName :: String  
+                     , age :: Int  
+                     , height :: Float  
+                     , phoneNumber :: String  
+                     , flavor :: String  
+                     } deriving (Show)
+
+add'     :: Int -> (Int -> Int)
+add' x y = x + y
